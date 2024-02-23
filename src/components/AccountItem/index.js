9013393fb2a763e1a136c20964748276fc19962a
quoc <quoctea@gmail.com>
2024-02-23 15:22:18 +0700

    Xây dựng UI phần Header #2

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+
+function AccountsItem() {
+  return (
+    <div className={cx('wrapper')}>
+      <img
+        className={cx('avatar')}
+        src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/72d2ffae35074be9c5877079262be6ca~c5_300x300.webp?lk3s=a5d48078&x-expires=1708840800&x-signature=yG%2F2vyX5INqOrTVl6G5LyLtzr7I%3D"
+        alt="gai xinh"
+      />
+      <div className={cx('info')}>
+        <h4 className={cx('name')}>
+          <span>Nguyen Van A</span>
+          <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+        </h4>
+        <span className={cx('username')}>nguyenvana</span>
+      </div>
+    </div>
+  );
+}
+
+export default AccountsItem;
